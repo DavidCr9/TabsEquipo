14935c9bb286b77bbed838c465c92331275f4df6
win603 <win603@S603-02.local>
2025-10-31 18:41:49 -0600

    Cambio a color azul

--- Tabs/ContentView.swift
+++ Tabs/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView {
+            Text("Welcome Screen")
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            
+            Text("Contador Screen")
+                .tabItem {
+                    Image(systemName: "clock")
+                    Text("Contador")
+                }
+        }.accentColor(.blue)
     }
 }
 
